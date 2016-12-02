34803cb650f9e91a569d5e561dcd89076cb12a00
gatorisgreater <william.broadway@gmail.com>
2016-12-02 12:52:05 -0500

    adding js file to repo.

---
+++ index.js
@@ -0,0 +1,27 @@
+var state = {
+  items: []
+};
+
+var item = {
+  name: "",
+  status: "",
+}
+
+function addItem (state, item) {
+  state.items.push(item);
+}
+
+function checkItem (state, item) {
+  state.items.push(item.status);
+  }
+
+function removeItem (state, item) {
+  state.items.remove[i];
+}
+
+
+for(var i=0;i<items.length;i++){
+  
+
+
+}
